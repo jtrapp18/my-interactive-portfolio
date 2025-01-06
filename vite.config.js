a8b89af6d8702eaa63086a7cfabd96c9fe0e8ca8
+ <jacqueline.trapp18@gmail.com>
2025-01-06 13:16:24 -0500

    reconfiguring base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-interactive-portfolio",
+  base: process.env.NODE_ENV === 'production' ? '/my-interactive-portfolio/' : '/',
   plugins: [react()],
 })
