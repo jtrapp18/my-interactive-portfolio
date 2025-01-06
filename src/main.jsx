5240314e06b72fd294a5184abb0b5455858643c8
+ <jacqueline.trapp18@gmail.com>
2025-01-06 14:04:43 -0500

    updating router for production

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import routes from "./routes";
 
-// const router = createBrowserRouter(routes, {
-//   basename: "/my-interactive-portfolio", // Add this line
-// });
+// const router = createBrowserRouter(routes);
 
-const router = createBrowserRouter(routes);
+const isProduction = process.env.NODE_ENV === 'production';
+const baseName = isProduction ? '/my-interactive-portfolio' : '/';
+
+const router = createBrowserRouter(routes, {
+  basename: baseName,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
