1e46ea20fa584f131e2b8104a0e691324c1e0a16
+ <jacqueline.trapp18@gmail.com>
2025-01-15 11:13:03 -0500

    revising data on technical about page

--- src/components/TechnicalHighlights.jsx
+++ src/components/TechnicalHighlights.jsx
@@ -12,11 +12,11 @@ const TechnicalHighlights = () => {
                 <p>
                     <strong>Python Highlights</strong>: scikit-learn, pandas, NumPy, matplotlib, boto3, streamlit, Flask, SQLAlchemy
                     <br/><br/>
-                    <strong>JavaScript Highlights</strong>: API requests with fetch, custom hooks, asynchronous programming (Promises, async/await), event handling and DOM manipulation
+                    <strong>JavaScript Highlights</strong>: API requests with fetch, asynchronous programming (Promises, async/await), event handling and DOM manipulation
                     <br/><br/>
-                    <strong>React Highlights</strong>: React Router, Context API, Material UI, JSX, components, and hooks
+                    <strong>React Highlights</strong>: React Router, Context API, Material UI, styled components, and custom hooks
                     <br/><br/>
-                    <strong>VBA Highlights</strong>: forms, connection across various Microsoft tools (Excel, Access, Word, Outlook)
+                    <strong>VBA Highlights</strong>: user forms, DAO connections, Add-ins using UDFs, connection across various Microsoft tools (Excel, Access, Word, Outlook)
                     <br/><br/>
                     <i>
                         For examples of how I applied these tools, visit the{" "}
