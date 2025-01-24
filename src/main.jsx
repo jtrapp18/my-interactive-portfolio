13df08b103ade0c3f8b261295744311bf2a3dcb7
+ <jacqueline.trapp18@gmail.com>
2025-01-24 17:29:39 -0500

    Revert "adding latest project"
    This reverts commit f87c2aeda8c6efbed8e7a831e438539fd3858ea8.
    
    reverting to working version

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import HashRouter
+import { createHashRouter, RouterProvider } from 'react-router-dom'; // Import HashRouter
 import routes from './routes'; // Import your routes configuration
 import { WindowWidthProvider } from './context/WindowWidthProvider';
 
 // Create the hash-based router
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 // Create the root and render the app with the router
 const root = ReactDOM.createRoot(document.getElementById('root'));
